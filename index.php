076f3a63b845649e662fac5c296f4252dd25ad53
llcain <llcain2012@gmail.com>
2024-02-28 12:05:15 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>What's Up World!</title>
+    </head>
+    <body>
+        
+    
+
+    <?php
+
+        $myvar = "This is my variable";
+        $name = "Bob";
+        echo "Hello, " . $name;
+    ?>    
+    </body>
+</html>
