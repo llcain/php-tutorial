653003a445b19db9fec8dfd09dda4800f5f81e22
llcain <llcain2012@gmail.com>
2024-02-28 13:16:44 -0600

    PHP variable

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <html>
     <head>
         <title>What's Up World!</title>
+        <link rel="stylesheet" href="styles.css">
     </head>
     <body>
         
+    <h1>PHP</h1>
+
+    <form action="process.php" method="post">
+       Enter your name: <input name="name" type="text">
+        <input type="submit">
+    </form>
     
 
-    <?php
-
-        $myvar = "This is my variable";
-        $name = "Bob";
-        echo "Hello, " . $name;
-    ?>    
+    
     </body>
 </html>
